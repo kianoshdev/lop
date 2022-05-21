7ebb1f05fdfc995f2267cbd4c3279d6d66b3ee94
kianosh iv <kianoshiv1733@gmail.com>
2022-05-21 18:08:48 +0430

    lop

---
+++ lop/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace lop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static string re(string input)
+            {
+                string rev = "";
+
+                char[] chr = input.ToCharArray();
+
+                int m = chr.Length - 1;
+
+                for (int i = 0; i <= input.Length - 1; i++)
+                {
+                    rev += chr[m];
+                    m--;
+                }
+                return rev;
+            }
+
+            int c = 5;
+
+            for (int i = 1; i <= 5; i++)
+            {
+                int u = 1;
+                string p = "";
+                string spac = @"";
+
+                while (u < i)
+                {
+                    p += u.ToString();
+                    u++;
+
+                }
+                for (int z = 1; z <= c; z++)
+                {
+                    spac += " ";
+                }
+
+                c--;
+
+                Console.WriteLine($"{spac}{p}{i}{re(p)}");
+            }
+        }
+    }
+}
